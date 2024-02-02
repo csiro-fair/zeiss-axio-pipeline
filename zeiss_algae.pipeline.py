e0a828d949382924a5fde835e35fd369f0043a34
Chris Jackett <chris.jackett@csiro.au>
2024-02-02 14:06:58 +1100

    Refactor filename extraction in zeiss_algae
    The filename extraction code in zeiss_algae.pipeline.py has been adjusted. The code now uses the name property of the file_name object to split and construct the new filename, ensuring the proper filename identifier is extracted and removed.

--- zeiss_algae.pipeline.py
+++ zeiss_algae.pipeline.py
@@ -237,7 +237,7 @@ class ZeissAxioObserver(BasePipeline):
         self.logger.info(f"Extracting frames...")
 
         # Remove the seventh filename identifier from the filename by splitting on underscore
-        file_name_parts = file_name.split("_")
+        file_name_parts = file_name.name.split("_")
         new_file_name = "_".join(file_name_parts[:5] + file_name_parts[7:])
 
         # # If CZI file has stacked images, fetch number of images
